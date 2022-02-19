1517f91fe0ee550ed6180abcfbacde0b40316e2e
Sl1ed <antaev2010@gmail.com>
2022-02-19 16:08:50 +0300

    Edited console options and parameters

--- src/main/java/mts/teta/resizer/ConsoleAttributes.java
+++ src/main/java/mts/teta/resizer/ConsoleAttributes.java
@@ -1,24 +1,159 @@
 package mts.teta.resizer;
 
-
 import java.io.File;
 
-public class ConsoleAttributes {
-    
-    protected File inputFile = null;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-    public void setInputFile(File file) {
+public class ConsoleAttributes {
+
+    private Integer[] resizeParams = new Integer[2];
+
+    @Option(
+            names = "--resize",
+            paramLabel = "width height",
+            description = "resize the image",
+            type = Integer.class
+    )
+    public void setResizeParams(Integer[] value) {
+        this.resizeParams = value;
     }
 
-    public void setOutputFile(File file) {
+    public Integer[] getResizeParams() {
+        return resizeParams;
     }
 
     public void setResizeWidth(Integer reducedPreviewWidth) {
+        resizeParams[0] = reducedPreviewWidth;
     }
 
     public void setResizeHeight(Integer reducedPreviewHeight) {
+        resizeParams[1] = reducedPreviewHeight;
     }
 
-    public void setQuality(int i) {
+    private int quality;
+
+    @Option(
+            names = "--quality",
+            paramLabel = "value",
+            description = "JPEG/PNG compression level",
+            type = Integer.class
+    )
+    public void setQuality(Integer quality) {
+        this.quality = quality;
+    }
+
+    private Integer[] cropParams = new Integer[4];
+
+    @Option(
+            names = "--crop",
+            paramLabel = "width height x y",
+            description = "cut out one rectangular area of the image",
+            type = Integer.class
+    )
+    public Integer[] getCrop() {
+        return cropParams;
+    }
+
+    public void setCrop(Integer[] crop) {
+        this.cropParams = crop;
+    }
+
+    public void setCropWidth(Integer cropWidth) {
+        cropParams[0] = cropWidth;
+    }
+
+    public void setCropHeight(Integer cropHeight) {
+        cropParams[1] = cropHeight;
+    }
+
+    public void setCropX(Integer cropX) {
+        cropParams[2] = cropX;
+    }
+
+    public void setCropY(Integer cropY) {
+        cropParams[4] = cropY;
+    }
+
+    private int blur;
+
+    @Option(
+            names = "--blur",
+            paramLabel = "{radius}",
+            description = "reduce image noise detail levels",
+            type = Integer.class
+    )
+    public void setBlur(Integer blur) {
+        this.blur = blur;
+    }
+
+    private String format;
+
+    @Option(
+            names = "--format",
+            paramLabel = "\"output format\"",
+            description = "the image format type",
+            type = String.class
+    )
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    File inputFile;
+
+    @Parameters(
+            index = "0",
+            hidden = true
+    )
+
+    public void setInputFile(File inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    private File outputFile;
+
+    @Parameters(
+            index = "1",
+            hidden = true
+    )
+
+    public void setOutputFile(File outputFile) {
+        this.outputFile = outputFile;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public Integer[] getCropParams() {
+        return cropParams;
+    }
+
+    public void setCropParams(Integer[] cropParams) {
+        this.cropParams = cropParams;
+    }
+
+    public int getBlur() {
+        return blur;
+    }
+
+    public void setBlur(int blur) {
+        this.blur = blur;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public File getInputFile() {
+        return inputFile;
+    }
+
+    public File getOutputFile() {
+        return outputFile;
     }
 }
--- src/main/java/mts/teta/resizer/ResizerApp.java
+++ src/main/java/mts/teta/resizer/ResizerApp.java
@@ -3,10 +3,23 @@ package mts.teta.resizer;
 import mts.teta.resizer.imageprocessor.ImageProcessor;
 import picocli.CommandLine;
 
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "resizer", mixinStandardHelpOptions = true, version = "resizer 0.0.1", description = "...")
+import static picocli.CommandLine.*;
+
+@Command(
+        name = "convert",
+        version = "resizer 1.0",
+        headerHeading = "Version: resizer 1.0 https://github.com/sl1ed256/CropCrop\n",
+        sortOptions = false,
+        separator = " ",
+        synopsisHeading = "Availble formats: jpeg png\n" +
+                "Usage: convert input-file [options ...] output-file\n",
+        customSynopsis = {"Option Settings:"}
+)
+
 public class ResizerApp extends ConsoleAttributes implements Callable<Integer> {
     public static void main(String... args) {
         int exitCode = runConsole(args);
@@ -14,13 +27,19 @@ public class ResizerApp extends ConsoleAttributes implements Callable<Integer> {
     }
 
     protected static int runConsole(String[] args) {
-        return new CommandLine(new ResizerApp()).execute(args);
+        return new CommandLine(new ResizerApp())
+                .setUsageHelpLongOptionsMaxWidth(40)
+                .execute(args);
     }
 
     @Override
     public Integer call() throws Exception {
         ImageProcessor imageProcessor = new ImageProcessor();
-        imageProcessor.processImage(ImageIO.read(inputFile), this);
+        try {
+            imageProcessor.processImage(ImageIO.read(inputFile), this);
+        } catch (IIOException e) {
+            throw new IIOException("Can't read input file");
+        }
         return 0;
     }
 
